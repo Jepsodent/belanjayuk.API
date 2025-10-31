e433035417165f5337ce3660124c249b79b5f256
Jefferson <jefferson.swanto@gmail.com>
2025-10-31 12:24:04 +0700

    init: Scaffolding db and setup db context

---
+++ belanjayuk.API/Models/Entities/DwmonthlySale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace belanjayuk.API.Models.Entities;
+
+public partial class DwmonthlySale
+{
+    public Guid Id { get; set; }
+
+    public string SellerId { get; set; } = null!;
+
+    public string MonthYear { get; set; } = null!;
+
+    public decimal? TotalSales { get; set; }
+
+    public int? TotalQty { get; set; }
+
+    public decimal? AvgRating { get; set; }
+
+    public DateTime? LoadDate { get; set; }
+}
